c9f81e9e67054912cd6ccc3ccb0c8a15caf54c52
lejeanf <jean-francois_robin@uqar.ca>
2023-08-15 19:27:21 -0400

    fixed compilation bug

--- Runtime/Editor/ReadOnlyDrawer.cs
+++ Runtime/Editor/ReadOnlyDrawer.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using UnityEngine;
 using UnityEditor;
 /// <summary>
@@ -26,3 +27,4 @@ public class ReadOnlyDrawer : PropertyDrawer
         GUI.enabled = previousGUIState;
     }
 }
+#endif
