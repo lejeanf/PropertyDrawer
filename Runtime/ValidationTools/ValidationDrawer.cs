553677ac37fc77d9fe98cf8ed479304e44ea63a1
lejeanf <jean-francois_robin@uqar.ca>
2024-01-30 15:58:35 -0500

    cleaning code

--- Runtime/ValidationTools/ValidationDrawer.cs
+++ Runtime/ValidationTools/ValidationDrawer.cs
@@ -9,12 +9,12 @@ namespace jeanf.validationTools
     [CustomPropertyDrawer(typeof(ValidationAttribute))]
     public class ValidationDrawer : PropertyDrawer
     {
-        private const int boxPadding = 10;
-        private const float padding = 10f;
-        private const float offset = 15f;
+        private const int BoxPadding = 10;
+        private const float Padding = 10f;
+        private const float Offset = 15f;
 
-        private float height = 10f;
-        private float helpBoxHeight = 0f;
+        private float _height = 10f;
+        private float _helpBoxHeight = 0f;
         
         
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
@@ -27,12 +27,12 @@ namespace jeanf.validationTools
                 var style = EditorStyles.helpBox;
                 style.alignment = TextAnchor.MiddleLeft;
                 style.wordWrap = true;
-                style.padding = new RectOffset(boxPadding, boxPadding, boxPadding, boxPadding);
+                style.padding = new RectOffset(BoxPadding, BoxPadding, BoxPadding, BoxPadding);
                 style.fontSize = 12;
 
-                helpBoxHeight = style.CalcHeight(new GUIContent(customAttribute.Text), Screen.width);
-                height = helpBoxHeight + propertyHeight + offset;
-                return height;
+                _helpBoxHeight = style.CalcHeight(new GUIContent(customAttribute.Text), Screen.width);
+                _height = _helpBoxHeight + propertyHeight + Offset;
+                return _height;
             }
             else
             {
@@ -47,13 +47,13 @@ namespace jeanf.validationTools
             {
                 var customAttribute = attribute as ValidationAttribute;
 
-                position.height = helpBoxHeight;
-                position.y += padding *.5f;
+                position.height = _helpBoxHeight;
+                position.y += Padding *.5f;
                 EditorGUI.HelpBox(position, customAttribute.Text, MessageType.Error);
-                position.height = height;
+                position.height = _height;
                 EditorGUI.DrawRect(position, new Color(1f, .2f, .2f, .1f));
 
-                position.y += helpBoxHeight + padding;
+                position.y += _helpBoxHeight + Padding;
                 position.height = base.GetPropertyHeight(property, label);
                 EditorGUI.PropertyField(position, property, new GUIContent(property.displayName));
             }
--- Runtime/ValidationTools/ValidationHighlighter.cs
+++ Runtime/ValidationTools/ValidationHighlighter.cs
@@ -10,10 +10,10 @@ namespace jeanf.validationTools
     [InitializeOnLoad]
     public class ValidationHighlighter
     {
-        private static readonly Color k_backgroundColor = new Color(0.7843f, 0.7843f, 0.7843f);
-        private static readonly Color k_backgroundProColor = new Color(0.2196f, 0.2196f, 0.2196f);
-        private static readonly Color k_backgroundSelectedColor = new Color(0.22745f, 0.447f, 0.6902f);
-        private static readonly Color k_backgroundSelectedProColor = new Color(0.1725f, 0.3647f, 0.5294f);
+        private static readonly Color BackgroundColor = new Color(0.7843f, 0.7843f, 0.7843f);
+        private static readonly Color BackgroundProColor = new Color(0.2196f, 0.2196f, 0.2196f);
+        private static readonly Color BackgroundSelectedColor = new Color(0.22745f, 0.447f, 0.6902f);
+        private static readonly Color BackgroundSelectedProColor = new Color(0.1725f, 0.3647f, 0.5294f);
 
         static ValidationHighlighter()
         {
@@ -24,32 +24,31 @@ namespace jeanf.validationTools
         {
             var isSelected = Selection.instanceIDs.Contains(instanceID);
             var obj = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
-            if (obj != null)
-            {
-                var validatable = obj.GetComponent<IValidatable>();
-                if (validatable is { IsValid: false })
-                {
-                    selectionRect.x += 16.5f; 
-                    var backgroundColor = EditorGUIUtility.isProSkin ? k_backgroundProColor : k_backgroundColor;
-                    if (isSelected)
-                        backgroundColor = EditorGUIUtility.isProSkin
-                            ? k_backgroundSelectedProColor
-                            : k_backgroundSelectedColor;
+            if (obj == null) return;
+            var validatable = obj.GetComponent<IValidatable>();
+            if (validatable is not { IsValid: false }) return;
+            selectionRect.x += 16f; 
+            selectionRect.y -= 1f;
+                    
+            var backgroundColor = EditorGUIUtility.isProSkin ? BackgroundProColor : ValidationHighlighter.BackgroundColor;
+            if (isSelected)
+                backgroundColor = EditorGUIUtility.isProSkin
+                    ? BackgroundSelectedProColor
+                    : BackgroundSelectedColor;
                     
 
-                    var textStyle = new GUIStyle(GUI.skin.label);
-                    textStyle.normal.textColor = new Color(1f,0.4705f,0.2196f);
+            var textStyle = new GUIStyle(GUI.skin.label);
+            textStyle.normal.textColor = new Color(1f,0.4705f,0.2196f);
 
-                    var width = textStyle.CalcSize(new GUIContent(obj.name)).x;
+            var width = textStyle.CalcSize(new GUIContent(obj.name)).x;
                     
-                    var backgroundRect = selectionRect;
-                    backgroundRect.width = width;
+            var backgroundRect = selectionRect;
+            backgroundRect.width = width;
+            backgroundRect.position = new Vector2(backgroundRect.position.x, backgroundRect.position.y + 1f);
                     
-                    EditorGUI.DrawRect(backgroundRect, backgroundColor);
-                    EditorGUI.LabelField(selectionRect, obj.name, textStyle);
-                    EditorApplication.RepaintHierarchyWindow();
-                }
-            }
+            EditorGUI.DrawRect(backgroundRect, backgroundColor);
+            EditorGUI.LabelField(selectionRect, obj.name, textStyle);
+            EditorApplication.RepaintHierarchyWindow();
         }
     }
     #endif
